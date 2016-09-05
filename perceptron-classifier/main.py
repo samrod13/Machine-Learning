908161ce698b9da95db77e16e860c10ea545f454
Samuel Rodriguez <rodriguezs466@gmail.com>
2016-09-04 22:08:28 -0500

    Function to visualize the decision boundaries

--- perceptron-classifier/main.py
+++ perceptron-classifier/main.py
@@ -4,7 +4,30 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 from perceptron import Perceptron
 
-def plot_decision_regions(x, y)
+
+def plot_decision_regions(X, y, classifier, resolution=0.02)
+# setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arrange(
+        x1_min, x1_max, resolution), np.arrange(x2_min, x2_max, resolution))
+    z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    z = z.reshape(xx1.shape)
+    plt.countourf(xx1, xx2, z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8,
+                    c=cmap(idx), marker=markers[idx], label=cl)
+
+
 # Read in the iris dataset
 df = pd.read_csv(
     'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
@@ -13,12 +36,12 @@ y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', -1, 1)
 x = df.iloc[0:100, [0, 2]].values
 #plt.scatter(x[:50, 0], x[:50, 1], color='red', marker='o', label='setosa')
-#plt.scatter(x[50:100, 0], x[50:100, 1], color='blue',
+# plt.scatter(x[50:100, 0], x[50:100, 1], color='blue',
 #            marker='x', label='versicolor')
 #plt.xlabel('petal length')
 #plt.ylabel('sepal length')
 #plt.legend(loc='upper left')
-#plt.show()
+# plt.show()
 
 # Training perceptron algorithm on the Iris dataset
 ppn = Perceptron(eta=0.1, n_iter=10)

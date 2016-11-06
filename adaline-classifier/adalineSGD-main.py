8c9b3fc195822499f64d18c694b92d074a0280cf
Samuel Rodriguez <rodriguezs466@gmail.com>
2016-11-05 22:48:02 -0500

    Added adalineSGD

--- adaline-classifier/adalineSGD-main.py
+++ adaline-classifier/adalineSGD-main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
 import pandas as pd
-from adaline import AdalineGD
+from adalineSGD import AdalineSGD
 
 
 def plot_decision_regions(X, y, classifier, resolution=0.02):
@@ -35,30 +35,16 @@ y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', -1, 1)
 x = df.iloc[0:100, [0, 2]].values
 
-# make the plot
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
-
-#ada1 = AdalineGD(n_iter=10, eta=0.01).fit(x,y)
-#ax[0].plot(range(1, len(ada1.cost_)+1),np.log10(ada1.cost_), marker = 'o')
-#ax[0].set_xlabel('Epochs')
-#ax[0].set_ylabel('log(Sum-squared-error)')
-#ax[0].set_title('Adaline - Learning Rate 0.01')
-
-#ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(x,y)
-#ax[1].plot(range(1, len(ada2.cost_)+1), ada2.cost_, marker='o')
-#ax[1].set_xlabel('Epochs')
-#ax[1].set_ylabel('Sum-squared-error')
-#ax[1].set_title('Adaline - Learning rate 0.0001')
 
 # standardizing instead
 x_std = np.copy(x)
 x_std[:, 0] = (x[:, 0] - x[:, 0].mean()) / x[:, 0].std()
 x_std[:, 1] = (x[:, 1] - x[:, 1].mean()) / x[:, 1].std()
 
-ada = AdalineGD(n_iter=15, eta=0.01)
+ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
 ada.fit(x_std, y)
 plot_decision_regions(x_std, y, classifier=ada)
-plt.title('Adaline - Gradient Descent')
+plt.title('Adaline - Stochastic Gradient Descent')
 plt.xlabel('sepal length [standardized]')
 plt.ylabel('petal length [standardized]')
 plt.legend(loc='upper left')
@@ -66,5 +52,5 @@ plt.show()
 
 plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
 plt.xlabel('Epochs')
-plt.ylabel('Sum-squared-error')
+plt.ylabel('Average Cost')
 plt.show()
